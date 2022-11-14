be7b4da4cdb337ca658b09b94a9c877dccb45f9b
Kemi Adeyemi <kembalos@hotmail.com>
2022-11-14 14:16:06 +0200

    ostoslista

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, Alert, TextInput, Image, Text, FlatList } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  const [thing, setThing] = useState('');
+  const [data, setData] = useState([]);
+
+  function addToList() {
+    setData([...data, { key: String(data.length), text: thing }]);
+    setThing('');
+  }
+
+  function clear() {
+    setData([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} placeholder="Write" onChangeText={text => setThing(text)} value={thing}/>
+      <View style={styles.buttons}>
+      <Button onPress={addToList} title="Add" />
+      <Button onPress={clear} title="Clear" />
+      </View>
+
+      
+        <FlatList
+          ListHeaderComponent={() => <Text style={{ color: 'blue' }}>Shopping List</Text>}
+          data={data}
+          renderItem={({ item }) => <Text>{item.text}</Text>}
+        />
+      
+
     </View>
   );
 }
@@ -15,6 +41,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    marginTop: 100
   },
+
+buttons: {
+  
+  flexDirection: 'row', 
+  alignItems: 'center', 
+  justifyContent: 'space-around'
+
+},
+
+  input : {
+    width:200  , 
+    borderColor: 'gray', 
+    borderWidth: 1
+    
+  }
 });
